58babd14855839c27e6716cdfd31fe410ffde3e7
mas0061 <mas0061@gmail.com>
2016-07-14 06:18:39 +0900

    minimum build env, hello world

---
+++ src/camp-recorder.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class CampRecorder extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello world!</div>
+    );
+  }
+
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CampRecorder from './camp-recorder.jsx';
+
+ReactDOM.render(
+  <CampRecorder />,
+  document.getElementById('cr-component')
+);
